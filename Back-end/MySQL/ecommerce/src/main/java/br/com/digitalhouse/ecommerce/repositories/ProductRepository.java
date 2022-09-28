465b484fd8ebdf0ad89f30c656666bc46fe82ca4
Anderson Borges <a.senaborges@gmail.com>
2022-09-28 02:55:16 -0300

    Back-end configurado com o perfil de desenvolvimento - MySQL

---
+++ Back-end/MySQL/ecommerce/src/main/java/br/com/digitalhouse/ecommerce/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package br.com.digitalhouse.ecommerce.repositories;
+
+import br.com.digitalhouse.ecommerce.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
---
+++ Back-end/MySQL/ecommerce/src/test/java/br/com/digitalhouse/ecommerce/tests/Factory.java
@@ -0,0 +1,18 @@
+//package br.com.digitalhouse.ecommerce.tests;
+//
+//import br.com.digitalhouse.ecommerce.dtos.CategoryDto;
+//import br.com.digitalhouse.ecommerce.entities.Category;
+//
+//public class Factory {
+//
+//    public static Category criarCategoria() {
+//        Category category = new Category(null, "Marketing");
+//        return category;
+//    }
+//
+//    public static CategoryDto criarCategoriaDto() {
+//        Category category = criarCategoria();
+//        return new CategoryDto(category);
+//    }
+//
+//}
